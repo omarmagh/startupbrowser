8a8668877691c05faacb9e78851084ba2066e9c9
omarm <omar.maghmoumeh@aiyexpertsolutions.com>
2025-01-22 11:48:58 +0200

    implement extracted types

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,5 @@
 import SearchForm from "@/components/SearchForm";
-import StartupCard from "@/components/StartupCard";
+import StartupCard, { StartupTypeCard } from "@/components/StartupCard";
 import { client } from "@/sanity/lib/client";
 import { STARTUPS_QUERY } from "@/sanity/lib/queries";
 
@@ -33,7 +33,7 @@ export default async function Home({
 
         <ul className="mt-7 card_grid">
           {posts?.length > 0 ? (
-            posts.map((post: StartupCardType, index: number) => <StartupCard key={post._id} post={post}/>)
+            posts.map((post: StartupTypeCard, index: number) => <StartupCard key={post._id} post={post}/>)
           ) : (
             <p className="no-results">No startups found</p>
           )}
