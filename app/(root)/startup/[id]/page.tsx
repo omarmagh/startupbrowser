12bfa28495e34917af7f08b6f8bb58bbf7ba5b3c
omarm <omar.maghmoumeh@aiyexpertsolutions.com>
2025-01-22 12:57:21 +0200

    implement experimental ppr and get data by id

---
+++ app/(root)/startup/[id]/page.tsx
@@ -0,0 +1,22 @@
+// /startup/:id/page.tsx
+import { client } from "@/sanity/lib/client";
+import { STARTUPS_BY_ID_QUERY } from "@/sanity/lib/queries";
+import { notFound } from "next/navigation";
+import React from "react";
+
+export const experimental_ppr = true;
+
+const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
+    const id = (await params).id;
+
+    const post = await client.fetch(STARTUPS_BY_ID_QUERY, { id });
+
+    if (!post) return notFound();
+
+    return (
+        <>
+            <h1 className="text-3xl">{post.title}</h1>
+        </>
+    );
+};
+export default Page;
--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -13,3 +13,18 @@ export const STARTUPS_QUERY = defineQuery(`*[_type == "startup" && defined(slug.
   category, 
   image
 }`);
+
+export const STARTUPS_BY_ID_QUERY = defineQuery(`*[_type == "startup" && _id==$id][0] {
+  _id, 
+  title,
+  slug,
+  _createdAt,
+  author -> {
+    _id, name, username, image, bio
+  }, 
+  views, 
+  description, 
+  category, 
+  image,
+  pitch
+}`);
