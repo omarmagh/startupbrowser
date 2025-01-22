4602ebd908bc88468dcc396c2846c6ba96e9b925
omarm <omar.maghmoumeh@aiyexpertsolutions.com>
2025-01-22 11:18:44 +0200

    fetch real db data from studio

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,7 @@
 import SearchForm from "@/components/SearchForm";
 import StartupCard from "@/components/StartupCard";
+import { client } from "@/sanity/lib/client";
+import { STARTUPS_QUERY } from "@/sanity/lib/queries";
 
 export default async function Home({
   searchParams,
@@ -8,21 +10,7 @@ export default async function Home({
 }) {
   const query = (await searchParams).query || "";
 
-  const posts = [
-    {
-      _createdAt: new Date(),
-      views: 55,
-      author: {
-        _id: 1,
-        name: "Omar",
-      },
-      _id: 1,
-      description: "This is a description",
-      image: "https://placehold.co/600x400",
-      category: "Robots",
-      title: "We Robots",
-    },
-  ];
+  const posts = await client.fetch(STARTUPS_QUERY);
 
   return (
     <>
