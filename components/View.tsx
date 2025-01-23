4bd55152806b8884e9b759419b7c07da1aeb10f8
Omar Maghmoumeh <omar.maghmoumeh@gmail.com>
2025-01-23 11:04:57 +0200

    implement working views counter (sanity write)

--- components/View.tsx
+++ components/View.tsx
@@ -1,16 +1,21 @@
 import { client } from "@/sanity/lib/client";
 import Ping from "./Ping";
 import { STARTUPS_VIEWS_QUERY } from "@/sanity/lib/queries";
+import { writeClient } from "@/sanity/lib/write-client";
+import { after } from "next/server";
 
 const View = async ({ id }: { id: string }) => {
     const viewsFormatter = () => {
         return `${totalViews} view` + (totalViews > 1 ? "s" : "");
     }
-    const { views: totalViews} = await client.withConfig({
+    const { views: totalViews } = await client.withConfig({
         useCdn: false
     }).fetch(STARTUPS_VIEWS_QUERY, { id });
 
-    // TODO: Update the number of views
+    after(async () => await writeClient
+        .patch(id)
+        .set({ views: totalViews + 1 })
+        .commit())
 
     return <div className="view-container">
         <div className="absolute -top-2 -right-2">
