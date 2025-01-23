cef5f2e8a8deed0b73b8ac3707a02c3fbeea0e98
Omar Maghmoumeh <omar.maghmoumeh@gmail.com>
2025-01-23 16:32:55 +0200

    transform "sequential" to "parallel" rendering

--- app/(root)/startup/[id]/page.tsx
+++ app/(root)/startup/[id]/page.tsx
@@ -18,9 +18,14 @@ export const experimental_ppr = true;
 const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
     const id = (await params).id;
 
-    const post = await client.fetch(STARTUPS_BY_ID_QUERY, { id });
+    const [post, { select: editorPosts }] = await Promise.all([
+        client.fetch(STARTUPS_BY_ID_QUERY, { id }),
+        client.fetch(PLAYLIST_BY_SLUG_QUERY, { slug: "editor-picks" })
+    ]);
 
-    const { select: editorPosts } = await client.fetch(PLAYLIST_BY_SLUG_QUERY, { slug: "editor-picks" });
+    // const post = await client.fetch(STARTUPS_BY_ID_QUERY, { id });
+
+    // const { select: editorPosts } = await client.fetch(PLAYLIST_BY_SLUG_QUERY, { slug: "editor-picks" });
 
     if (!post) return notFound();
 
