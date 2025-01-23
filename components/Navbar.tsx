02e923a2b4d6278f293ed57d47eaa41715f7f310
Omar Maghmoumeh <omar.maghmoumeh@gmail.com>
2025-01-23 16:09:18 +0200

    finalize user profile

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,54 +1,65 @@
 import Link from "next/link";
 import Image from "next/image";
 import { auth, signOut, signIn } from "@/auth";
-import { Button } from "./ui/button";
+import { BadgePlus, LogOut } from "lucide-react";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 const Navbar = async () => {
-    const session = await auth();
-    return (
-        <header className="px-5 py-3 bg-white shadow-sm font-work-sans">
-            <nav className="flex justify-between items-center">
-                <Link href="/">
-                    <Image src="/logo.png" alt="Logo" width={144} height={30} />
-                </Link>
+  const session = await auth();
 
-                <div className="flex items-center gap-5 text-black">
-                    {session && session.user ? (
-                        <>
-                            <Link href="/startup/create">
-                                <span>Create</span>
-                            </Link>
-                            <form action={async () => {
-                                    "use server";
-                                    await signOut({redirectTo: '/'});
-                                }
-                            }>
-                                <Button type="submit">
-                                    Logout
-                                </Button>
-                            </form>
+  return (
+    <header className="px-5 py-3 bg-white shadow-sm font-work-sans">
+      <nav className="flex justify-between items-center">
+        <Link href="/">
+          <Image src="/logo.png" alt="logo" width={144} height={30} />
+        </Link>
 
-                            <Link href={`/user/${session?.id}`}>
-                                <span>{session?.user.name}</span>
-                            </Link>
-                        </>
-                    ) : (
-                        <>
-                            <form action={async () => 
-                                {
-                                    "use server";
-                                    await signIn('github');
-                                }
-                            }>
-                                <Button type="submit">
-                                    Login
-                                </Button>
-                            </form>
-                        </>
-                    )}
-                </div>
-            </nav>
-        </header>
-    )
-}
-export default Navbar
+        <div className="flex items-center gap-5 text-black">
+          {session && session?.user ? (
+            <>
+              <Link href="/startup/create">
+                <span className="max-sm:hidden">Create</span>
+                <BadgePlus className="size-6 sm:hidden" />
+              </Link>
+
+              <form
+                action={async () => {
+                  "use server";
+
+                  await signOut({ redirectTo: "/" });
+                }}
+              >
+                <button type="submit">
+                  <span className="max-sm:hidden">Logout</span>
+                  <LogOut className="size-6 sm:hidden text-red-500" />
+                </button>
+              </form>
+
+              <Link href={`/user/${session?.id}`}>
+                <Avatar className="size-10">
+                  <AvatarImage
+                    src={session?.user?.image || ""}
+                    alt={session?.user?.name || ""}
+                  />
+                  <AvatarFallback>AV</AvatarFallback>
+                </Avatar>
+              </Link>
+            </>
+          ) : (
+            <form
+              action={async () => {
+                "use server";
+
+                await signIn("github");
+              }}
+            >
+              <button type="submit">Login</button>
+            </form>
+          )}
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -53,3 +53,17 @@ export const AUTHOR_BY_ID_QUERY = defineQuery(`*[_type == "author" && _id==$id][
   image,
   bio 
 }`);
+
+export const STARTUPS_BY_AUTHOR_QUERY = defineQuery(`*[_type == "startup" && author._ref == $id] | order(_createdAt desc) {
+  _id, 
+  title,
+  slug,
+  _createdAt,
+  author -> {
+    _id, name, image, bio
+  }, 
+  views, 
+  description, 
+  category, 
+  image
+}`);
